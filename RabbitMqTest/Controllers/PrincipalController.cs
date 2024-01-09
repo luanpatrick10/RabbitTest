3b6f3dc290c5bb2571b07745a768d65a51e27ea9
Luan <luanpck@hotmail.com>
2024-01-08 22:25:09 -0300

    Primeiro

---
+++ RabbitMqTest/Controllers/PrincipalController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMqTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrincipalController : ControllerBase
+    {
+        [HttpGet("publicar")]
+        public IActionResult PublicarMensagem(string mensagem)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "saudacao_1",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+                string message = mensagem;
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "saudacao_1", basicProperties: null, body: body);
+            }
+            return Ok("Mensagem");
+        }
+    }
+}
---
+++ RabbitMqTestConsumer/RabbitMqTestConsumer/Controllers/PrincipalController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMqTestConsumer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrincipalController : ControllerBase
+    {
+        [HttpGet("consumir")]
+        public IActionResult Consumir()
+        {
+            string mensagem = "";
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "saudacao_1",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    mensagem = message;
+                };
+                channel.BasicConsume(queue: "saudacao_1", autoAck: true, consumer: consumer);
+                Task.Delay(1000).Wait();
+            }
+            return Ok(mensagem);
+        }
+
+    }
+}
